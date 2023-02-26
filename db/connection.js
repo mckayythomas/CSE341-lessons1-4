819d71286c0b64c6f93f293d57cb8874acbda34c
mckayythomas <mckayycollins@gmail.com>
2023-02-25 23:53:22 -0500

    second try

--- db/connection.js
+++ db/connection.js
@@ -1,62 +1,62 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const {MongoClient} = require('mongodb')
+// const {MongoClient} = require('mongodb')
 
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
+// async function main(){
+//     /**
+//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//      */
 
 
-    const client = new MongoClient(process.env.MONGO_URI);
+//     const client = new MongoClient("mongodb+srv://mckayycollins:PswEzQTDabKry4oI@week1-4cluster.wqolsip.mongodb.net/?retryWrites=true&w=majority");
  
 
-    await client.connect();
+//     await client.connect();
 
-    // try {
-    //     // Connect to the MongoDB cluster
-    //     await client.connect();
-    //     console.log("Database connection made..!") 
-    // } catch (e) {
-    //     console.error(e);
-    // } finally {
-    //     await client.close();
+//     // try {
+//     //     // Connect to the MongoDB cluster
+//     //     await client.connect();
+//     //     console.log("Database connection made..!") 
+//     // } catch (e) {
+//     //     console.error(e);
+//     // } finally {
+//     //     await client.close();
         
-    // }
+//     // }
 
-    return client;
+//     return client;
+// };
+
+// let client = main();
+
+// main().catch(console.error);
+
+// module.exports = client;
+
+const dotenv = require('dotenv');
+dotenv.config();
+const MongoClient = require('mongodb').MongoClient;
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect("mongodb+srv://mckayycollins:PswEzQTDabKry4oI@week1-4cluster.wqolsip.mongodb.net/?retryWrites=true&w=majority")
+
 };
 
-let client = main();
+const getDb = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
+};
 
-main().catch(console.error);
-
-module.exports = client;
-
-// const dotenv = require('dotenv');
-// dotenv.config();
-// const MongoClient = require('mongodb').MongoClient;
-
-// let _db;
-
-// const initDb = (callback) => {
-//   if (_db) {
-//     console.log('Db is already initialized!');
-//     return callback(null, _db);
-//   }
-//   MongoClient.connect(process.env.MONGODB_URI)
-
-// };
-
-// const getDb = () => {
-//   if (!_db) {
-//     throw Error('Db not initialized');
-//   }
-//   return _db;
-// };
-
-// module.exports = {
-//   initDb,
-//   getDb,
-// };
+module.exports = {
+  initDb,
+  getDb,
+};
